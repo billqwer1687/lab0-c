f1a179a696f4aede2abac6602d70fb8f84c7f68b
billqwer1687 <billqwer1687@gmail.com>
2021-07-13 00:47:41 +0800

    Implement queue insert head function Use malloc to allocate space to string and copy the string to node.

--- queue.c
+++ queue.c
@@ -37,11 +37,19 @@ bool q_insert_head(queue_t *q, char *s)
 {
     list_ele_t *newh;
     /* TODO: What should you do if the q is NULL? */
+    if (!q) {
+        return false;
+    }
     newh = malloc(sizeof(list_ele_t));
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
+    newh->value = malloc((strlen(s) + 1) * sizeof(char));
+    memset(newh->value, '\0', strlen(s) + 1);
+    strncpy(newh->value, s, strlen(s));
     newh->next = q->head;
     q->head = newh;
+    q->size++;
+
     return true;
 }
 
